85181ab3b713bc6cb81a770274a5df6469e04327
BlooMaan <BlooMaan@users.noreply.github.com>
2017-07-19 12:17:38 +0100

    Add files via upload

--- scraper.js
+++ scraper.js
@@ -1,14 +1,30 @@
-//Requirements
-var request = require('request');
+/*~~~Requirements~~~*/
+const request = require('request');
+const fs = require('fs');
+
+/*~~~Global Variables~~~*/
+//Get regexs used for searching webpage from the json file
+var regexpress = JSON.parse(fs.readFileSync('scraperRegexs.json'));
 
 //Object to store data taken from the website
-var InfoObj = {
-	telephone:[],
-	email:[],
-	postcode:[],
-};
+var InfoObj = {};
+//Generate object using regexpress object
+for (let key in regexpress){
+	InfoObj[key] =[];
+}
 
-//function to scrape data from element text using a regular expression (regex) and store the data into a storage array
+//take email address
+var email = 'tim@canddi.com';
+
+//Extract web address from email and format
+const website = 'http://' + email.split('@')[1];
+
+
+/*~~~Functions~~~*/
+/*
+Summary:   Scrapes data elementText string using a regular expression stores the data in an array
+Arguments: The string to scrape data from | Array to store scraped data in | regex used to identify data
+*/
 function scrapeRegex(elementText, storageArray, regex){
 	//store array of regex matches
 	var matchlist = elementText.match(regex);
@@ -19,20 +35,22 @@ function scrapeRegex(elementText, storageArray, regex){
 	}
 };
 
+/*
+Summary:   Generic data collecting function for a request. Won't look for other pages, focuses purely getting data.
+Arguments: Error info (when applicable) | Response object | retrieved data (string)
+*/
+function genericDataSearch(err, response, body){
+	//Scrape info for each key in infoObj
+	for (let key in InfoObj){
+		scrapeRegex(body,InfoObj[key],new RegExp(regexpress[key][0],regexpress[key][1]));
+	}
+}
 
-request({
-	method: 'GET',
-	url: 'http://canddi.com/'
-},function(err, response, body) {
+/*~~~Page request and scraping~~~*/
+request(website,function (err, response, body){
 	if(err) return console.error(err);
 
-	//Scrape...
-	//Phone numbers
-	scrapeRegex(body,InfoObj.telephone,/(\+\d{1,3}\s?(\s\(0\))?|0)(\d{3}\s?\d{3}\s?\d{4}|\d{4}\s?\d{6})(?![0-9])/g);
-	//Email addresses
-	scrapeRegex(body, InfoObj.email, /[A-Za-z][A-Za-z0-9._%+-]+@[A-Za-z0-9-]+(\.[A-Za-z]{3}|(\.[A-Za-z]{2}){2})/g);
-	//Post codes
-	scrapeRegex(body,InfoObj.postcode, /[A-Z]{1,2}(([0-9]{1,2})|([0-9][A-Z]))\s[0-9][A-Z]{1,2}/g);
+	genericDataSearch(err, response, body);
 
 	//Print object to console
 	console.log(InfoObj);
