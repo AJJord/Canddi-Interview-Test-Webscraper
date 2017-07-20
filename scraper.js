a82b23505fb453f06d8799e007818e616d888664
BlooMaan <BlooMaan@users.noreply.github.com>
2017-07-20 14:32:22 +0100

    Add files via upload

--- scraper.js
+++ scraper.js
@@ -1,6 +1,8 @@
 /*~~~Requirements~~~*/
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+const cheerio = require('cheerio');
 
 /*~~~Global Variables~~~*/
 //Get regexs used for searching webpage from the json file
@@ -13,11 +15,7 @@ for (let key in regexpress){
 	InfoObj[key] =[];
 }
 
-//take email address
-var email = 'tim@canddi.com';
 
-//Extract web address from email and format
-const website = 'http://' + email.split('@')[1];
 
 
 /*~~~Functions~~~*/
@@ -39,7 +37,7 @@ function scrapeRegex(elementText, storageArray, regex){
 Summary:   Generic data collecting function for a request. Won't look for other pages, focuses purely getting data.
 Arguments: Error info (when applicable) | Response object | retrieved data (string)
 */
-function genericDataSearch(err, response, body){
+function genericDataSearch(body){
 	//Scrape info for each key in infoObj
 	for (let key in InfoObj){
 		scrapeRegex(body,InfoObj[key],new RegExp(regexpress[key][0],regexpress[key][1]));
@@ -47,11 +45,44 @@ function genericDataSearch(err, response, body){
 }
 
 /*~~~Page request and scraping~~~*/
-request(website,function (err, response, body){
-	if(err) return console.error(err);
+//Get email address
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-	genericDataSearch(err, response, body);
+var email = "example@canddi.com";//Set default value
 
-	//Print object to console
-	console.log(InfoObj);
+rl.question('Enter an email address (default {0}): '.replace('{0}', email), function (answer){
+	if (answer.trim() != ""){//if input given
+		email = answer;
+		//Get email RegEx
+		let emailRegExp = new RegExp(regexpress.email[0], regexpress.email[1]);
+		//Test if string is valid
+		if (!emailRegExp.test(email)){
+			rl.close();
+			return console.log('Input does not match formatting for an email address');
+		}
+	}
+	rl.close();
+
+	//Extract web address from email and format
+	const website = 'http://' + email.split('@')[1];
+
+	request(website,function (err, response, body){
+		if(err) return console.error(err);
+		//Perform a pass with plain html
+		genericDataSearch(body);
+		//perform a pass with visible text extracted by cheerio
+		const $ = cheerio.load(body);
+		$('body').each(function(){
+			let tagText = $(this).text();
+			// console.log(tagText);
+			genericDataSearch(tagText);
+		});
+	});
+});
+
+process.on('exit', (code)=>{
+	console.log(InfoObj)
 });
